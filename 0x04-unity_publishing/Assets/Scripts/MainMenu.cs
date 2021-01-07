bf289d0b9a3057449a90ceb3dac85ffb15721042
Kefi <1390@holbertonschool.com>
2021-01-07 23:42:16 +0100

    Updated

---
+++ 0x04-unity_publishing/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    // goal material
+
+    public Material goalMat;
+   
+    // trap material
+    public Material trapMat;
+   
+    // color blind option
+    public Toggle colorblindMode;
+
+    void Start()
+    {
+        
+    }
+
+    public void PlayMaze()
+    {
+
+        if (colorblindMode.isOn == true)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = new Color32(255, 0, 0, 0);
+            goalMat.color = new Color32(0, 255, 0, 1);
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitMaze()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+}
---
+++ 0x04-unity_publishing/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private float gravityValue = -9.81f;
+    public float playerSpeed = 2.0f;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private int score;
+    public int health = 5;
+    private Scene maze;
+    public Text ScoreText;
+    public Text healthText;
+    public Text youWinLose;
+    public Image bg;
+    void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+       // maze = SceneManager.GetActiveScene();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0)
+        {
+           // Debug.Log("Game Over!");
+            health = 5;
+            score = 0;
+           // SceneManager.LoadScene(maze.name);
+            bg.color = Color.red;
+            youWinLose.text = "Game Over!".ToString();
+            youWinLose.color = Color.white;
+            bg.gameObject.SetActive(true);
+            StartCoroutine(LoadScene(3));
+        }
+        IEnumerator LoadScene(float seconds)
+        {
+            yield return new WaitForSeconds(seconds);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+        }
+
+    }
+    void FixedUpdate()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+    }
+    void SetScoreText()
+    {
+        ScoreText.text = "Score: " + score.ToString();
+    }
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            score++;
+            Destroy(other.gameObject);
+            SetScoreText();
+            //Debug.Log($"Score: {score}");
+        }
+        if (other.tag == "Trap")
+        {
+            health--;
+            SetHealthText();
+            // Debug.Log($"Health: {health}");
+        }
+        if (other.tag == "Goal")
+        {
+           // Debug.Log("You win!");
+        }
+        if (other.gameObject.tag == "Goal")
+        {
+            bg.color = Color.green;
+            youWinLose.text = "You Win!".ToString();
+            youWinLose.color = Color.black;
+            bg.gameObject.SetActive(true);
+            StartCoroutine(LoadScene(3));
+        }
+        IEnumerator LoadScene(float seconds)
+        {
+            yield return new WaitForSeconds(seconds);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+        }
+
+    }
+}
