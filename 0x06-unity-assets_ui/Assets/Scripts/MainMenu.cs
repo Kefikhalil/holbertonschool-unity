ac7891b06805de05aa1e431ee3aee7955c0f9912
Kefi <57833470+Kefikhalil@users.noreply.github.com>
2021-02-26 01:55:25 +0100

    updated

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    public void LevelSelect(int level)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void SecondLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void ThirdLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exited");
+    }
+}
