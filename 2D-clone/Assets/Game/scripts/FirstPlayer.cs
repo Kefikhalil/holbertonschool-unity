0ab446cacb6b294ea3cd7d1427bb13e0d5979361
Kefi <57833470+Kefikhalil@users.noreply.github.com>
2021-03-02 03:29:02 +0100

    2D Clone

---
+++ 2D-clone/Assets/Game/scripts/FirstPlayer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class FirstPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool canBoost;
+    public GameObject laserPrefab;
+    
+    // fire rate is 0.5f
+    // Can fire -- has the amount of time between firing passed?
+    //Time.time
+    public float fireRate = 0.5f;
+    public float canFire = 0.0f;
+
+    [SerializeField]
+    public float speed = 5.0f;
+
+    private void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Movement();
+		 if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(1))
+		 {
+            if (Time.time > canFire)
+            {
+                Instantiate(laserPrefab, transform.position + new Vector3(0, 1f, 0), Quaternion.identity);
+                canFire = Time.time + fireRate;
+            }
+            
+		 }
+	}
+	private void Movement()
+{
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+    // zone out
+    if (transform.position.x > 9.5f)
+    {
+        transform.position = new Vector3(-9.5f, transform.position.y, 0);
+    }
+    else if (transform.position.x < -9.5f)
+    {
+        transform.position = new Vector3(9.5f, transform.position.y, 0);
+    }
+
+    if (transform.position.y > 0)
+    {
+        transform.position = new Vector3(transform.position.x, 0, 0);
+    }
+    else if (transform.position.y < -4.2f)
+    {
+        transform.position = new Vector3(transform.position.x, -4.2f, 0);
+    }
+}
+}
---
+++ 2D-clone/Assets/Game/scripts/Laser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move up at 10 speed
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        if (transform.position.y >= 6)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
