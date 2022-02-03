34aa6e6bc43f133fbd15764e8219d3081f3d78d9
Kefi <57833470+Kefikhalil@users.noreply.github.com>
2022-02-03 03:27:27 +0100

    updated

---
+++ 0x09-unity_ar_business_card/Assets/OpenUrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void facebook()
+    {
+        Application.OpenURL("https://www.facebook.com/profile.php?id=100017130693739");
+    }
+    public void github()
+    {
+        Application.OpenURL("https://github.com/Kefikhalil");
+    }
+    public void linkedin()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/khalil-kefi/");
+    }
+}
